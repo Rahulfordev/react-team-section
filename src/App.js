57407bafbf3a19ecd85da94db408d86c9ca4112a
rahul ali <82302787+Rahulfordev@users.noreply.github.com>
2022-10-19 12:09:36 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+import Services from "./components/Services";
+import './App.css';
+ 
+
+function App() {
+   return (
+    <div>
+     <Services />
+    </div>
+   )
+ 
+}
+
+export default App;
